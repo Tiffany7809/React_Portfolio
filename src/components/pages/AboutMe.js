080e49a85c1640ec35e32f51e9629df74cd68a52
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-07-17 14:35:20 -0700

    adding selfie image

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React from "react";
+import image from "../../../public/assets/images/"
 
 const style = {
-    h1: {
-        textAlign: 'center',
-    }
+  h1: {
+    textAlign: "center",
+  },
+  p: {
+    textAlign: 'Center',
+    borderColor: "black",
+    borderWidth: "3px",
+    borderStyle: "solid",
+    padding: "10px",
+    lineHeight: 2
+  }
 };
 
 export default function AboutMe() {
   return (
     <div>
       <h1 style={style.h1}>About Me</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+        <div>
+            <img className="selfie" src= {'../../../public/assets/images/Selfie.jpg'} 
+     alt="Tiffanys Selfie"/>
+        </div>
+
+      <p style={style.p}>
+            I started coding when I was in highschool, just small things for fun,
+        mostly drawing pictures with CSS. After highschool I decided to take on a Computer Science
+        major at Western Washington University. I completed my first year at WWU with an offer to join 
+        the honor roll for my 3.9 GPA. 
+            I am Currently attending th eUW coding Bootcamp to gain more confidence in my code
+        and to learn the most current technologies that will make me an asset to any team.
+            I am always an over achiver and love to code, I can spend hours on side projects and 
+        am looking into learnign Game Development. I love the constant challenge that this field offers 
+        me. 
+            Making this portfolio os my first step into working with React.js and I love it!
+
+            
+        
+        </p>
     </div>
   );
 }
