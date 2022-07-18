f6a52e83e38e73f990eb7a38e6015907719676a2
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-07-18 01:10:26 -0700

    CSS

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,14 +14,13 @@ export default function Home() {
     <div>
       <h1 style={style.h1}>Tiffany Dempsey</h1>
 
-      <p>
-        Hello! My name is Tiffany,I am a Full-Stack Developer. Welcome to my
-        React Porfolio. `I am a UW Coding Boot Camp student, working to expand
-      </p>
-      <p>my skill set in the tech field. I hope to be a front end web developer,
-        and someday work my way to game develpment as well. On this site, you
-        can check out some of the work I have done!
-      </p>
+      <p>Hello! </p>
+      <p> My name is Tiffany, I am a Full-Stack Developer.</p>
+      <p> Welcome to my React Porfolio.</p>
+      <p>I am a UW Coding Boot Camp student, working to expand</p>
+      <p> my skill set in the tech field. I hope to be a front end web developer,</p>
+      <p> and someday work my way to game develpment as well. On this site, you</p>
+      <p>can check out some of the work I have done!</p>
     </div>
   );
 }
