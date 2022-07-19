7eb5a8a971024b261d1a68c2380adc8541481684
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-07-19 00:26:07 -0700

    emailjs formatting

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 // import emailjs from '@emailjs/browser';
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 
 export default function ContactMe() {
   const form = useRef();
@@ -8,25 +8,30 @@ export default function ContactMe() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('gmail', 'Ytemplate_xax51bg', form.current, 'OZ9n9zpGVLcmPRD7a')
-      .then((result) => {
+    emailjs
+      .sendForm("gmail", "Ytemplate_xax51bg", form.current, "OZ9n9zpGVLcmPRD7a")
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
+      e.target.reset();
   };
 
   return (
     <div>
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
-};
+}
