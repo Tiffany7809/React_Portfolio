548236cbd307f6bd32af2b8200fd2b341de49e40
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-07-20 18:52:02 -0700

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-#React_Portfolio
-
-deployed application: 
-https://tiffany7809.github.io/React_Portfolio/
--- src/App.js
+++
@@ -1,8 +0,0 @@
-// export default App;
-import React from "react";
-import './styles/HeaderNav.css';
-import PortfolioContainer from "./components/PortfolioContainer";
-
-const App = () => <PortfolioContainer />;
-
-export default App;
