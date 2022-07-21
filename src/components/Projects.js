6d4abe0dc7173f0be6e588724457c9f64d8c6eff
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-07-20 22:15:44 -0700

    adding photos and resume download

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,9 +3,10 @@ import Card from "./Card";
 import TAI from "../images/translationapp.jpg";
 import CCI from "../images/communityCloset.png";
 import TBI from "../images/techBlog.png";
-import DPI from "../images/dayPlanner.png"
-import RPI from "../images/randomPass.png"
-
+import DPI from "../images/dayPlanner.png";
+import RPI from "../images/randomPass.png";
+import CQI from "../images/CodeQuiz.png";
+import ComingSoon from "../images/image-coming-soon.jpg";
 const projects = [
   {
     title: "World Traveler App",
@@ -14,7 +15,7 @@ const projects = [
             to translate words and phrases quickly on the go.`,
     deployedLink: "https://levisgaragegroupinc.github.io/translation_app/",
     id: 1,
-    img:`${TAI}`,
+    img: `${TAI}`,
     github: "https://github.com/levisgaragegroupinc/translation_app",
   },
   {
@@ -25,20 +26,30 @@ const projects = [
                 can give the clothes new life.`,
     deployedLink: "https://community-closet-206.herokuapp.com/",
     id: 2,
-    img:`${CCI}`,
+    img: `${CCI}`,
     gihub: "https://github.com/areitan/Community_Closet",
   },
   {
     title: "Code quiz",
-    description: `add description!`,
+    description: `The goal of this project is to create a code quiz that: 1. has a time limit 2. 
+            has a high score board that can be cleared by the user 3. 
+            cycles through multiple screens when the answers are given 4. 
+            subtracts 10 seconds when the wrong answer is given`,
     deployedLink: "https://tiffany7809.github.io/Code_Quiz/",
     id: 3,
-    img: "",
+    img: `${CQI}`,
     github: "https://github.com/Tiffany7809/Code_Quiz",
   },
   {
     title: "work Day Planner",
-    description: `add description!`,
+    description: `The Goal of This site is to create a day planner, 
+            that gives the user options to schedual their daily 
+            tasks in the typical work hours of 9 - 5.
+            The current date and time will be shown at the top of the page.
+            each row will show a different time block in the day that the user can enter text into.
+            each row should show if time has passed, is current or is in the future by color coding.
+            each time bloack will have its own save button.
+            when the user clicks the save button, the users input will be saved to the local storage.`,
     deployedLink: "https://tiffany7809.github.io/Day_Planner/",
     id: 4,
     img: `${DPI}`,
@@ -46,7 +57,11 @@ const projects = [
   },
   {
     title: "Random Password Generator",
-    description: `add description!`,
+    description: `The goal of this project, was to use Java Script to create a Random Password Generator. 
+            When the generate password button is clicked, it will ask what the length of  
+            the password should be, and if the user would like to include, upper case letter, 
+            lower case letters, numbers and symbols. Depending on the users choices, the password generator 
+            will produce a password meeting those parameters.`,
     deployedLink: "https://tiffany7809.github.io/Random_Password_Generator/",
     id: 5,
     img: `${RPI}`,
@@ -54,7 +69,8 @@ const projects = [
   },
   {
     title: "Tech Blog",
-    description: `add description!`,
+    description: `This is a full stack application, a blog that users can post and view others posts as well as edit and delete posts. 
+            This apploication uses Handlebars,Express,NodeJS,Bootstrap,Sequelize,Password Authenitcation HTML, JS and CSS.`,
     deployedLink: "https://techblogtiffany.herokuapp.com/",
     id: 6,
     img: `${TBI}`,
@@ -62,10 +78,12 @@ const projects = [
   },
   {
     title: "PWA Text Editor",
-    description: `add description!`,
+    description: `This application is a text editor that can be downloaded as an app on mulpiple devices.
+          Progressive Web Applications allow for users to enjoy the application 
+          even while not connected to the internet so long as they have installed the application their device.`,
     deployedLink: "https://text-editor-tiffany.herokuapp.com/",
     id: 7,
-    img: "",
+    img: `${ComingSoon}`,
     github: "https://github.com/Tiffany7809/Text_Editor_App",
   },
 ];
@@ -73,7 +91,7 @@ const projects = [
 export default function Project() {
   return (
     <div>
-      {projects.map(({img, title, description, deployedLink, id, github }) => (
+      {projects.map(({ img, title, description, deployedLink, id, github }) => (
         <Card
           img={img}
           title={title}
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,22 +5,61 @@ const style = {
   h1: {
     fontWeight: "600",
   },
+  h2: {
+    textAlign: "center",
+    fontFamily: "'Arima', cursive",
+    fontWeight: "700",
+    textDecoration: "underline",
+  },
+  h3: {
+    fontFamily: "'Arima', cursive",
+    fontWeight: "700",
+    textDecoration: "underline",
+  },
+  ul: {
+    listStyle: "none",
+    fontFamily: "'Arima', cursive",
+    fontWeight: "600"
+  },
 };
 
 export default function Resume() {
   return (
-    <div>
+    <div style={style.div}>
       <h1 style={style.h1}>Resume</h1>
-      <p>Download my full Resume: <a>Download</a></p>
-      
       <>
-      Name:
-      Front End Proficiencies:
-      Back end Proficiencies 
+        <h2 style={style.h2}>Tiffany Dempsey</h2>
+        <p>
+          Download my full Resume: <a href="\assets\Tiffany Dempsey Resume.pdf" download="Tiffany Dempsey Resume.pdf">Download</a>
+        </p>
+        <br></br>
+        <h3 style={style.h3}>Front End Proficiencies:</h3>
+        <ul style={style.ul}>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>ReactJs</li>
+          <li>Bootstrap</li>
+          <li>Foundation</li>
+          <li>Figma</li>
+        </ul>
+        <br></br>
+        <h3 style={style.h3}>Back end Proficiencies </h3>
+        <ul style={style.ul}>
+          <li>APIs </li>
+          <li>NodeJS</li>
+          <li> OOP</li>
+          <li>Express</li>
+          <li>SQL</li>
+          <li>NoSQL</li>
+          <li>GraphQL</li>
+        </ul>
+        <h3 style={style.h3}>Education</h3>
+        <ul style = {style.ul}>
+          <li>UW Coding Bootcamp</li>
+          <li>Western Washington University</li>
+        </ul>
       </>
-  
-      
-
     </div>
   );
 }
